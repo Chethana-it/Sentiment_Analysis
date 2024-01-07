4dc4eea8a34f14ac084dc0fd501c3e45409a4642
Chethana Jayasinghe <63340994+Chethana-it@users.noreply.github.com>
2024-01-07 22:41:16 +0530

    integrate prediction pipeline to frontend

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+from helper import preprocessing, vectorizer, get_prediction
 
 app = Flask(__name__)
 
 
 data = dict()
-reviews = ['Good Product', 'Bad Product', 'I like It']
+# reviews = ['Good Product', 'Bad Product', 'I like It']
+reviews = []
 
-possitive = 2
-negative = 1
+possitive = 0
+negative = 0
 
 
 @app.route("/")
@@ -20,7 +22,19 @@ def index():
 @app.route("/", methods = ['post'])
 def my_post():
     text = request.form["text"]
+    preprocessed_text = preprocessing(text)
+    vectorized_text = vectorizer(preprocessed_text)
+    prediction = get_prediction(vectorized_text)
 
+    if prediction == 'negative':
+        global negative
+        negative += 1
+    else:
+        global possitive
+        possitive += 1
+
+    reviews.insert(0, text)
+    return redirect(request.url)
 
 if __name__ == "__main__":
     app.run()
